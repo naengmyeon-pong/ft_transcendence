57d599f2ac7b55b0437ac6530d196b5d947a3dc5
joonhan <joonhan@student.42seoul.kr>
2023-07-12 12:47:05 +0900

    🔧 chore: Add eslint and prettier #1

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["google", "plugin:react/recommended", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "html"],
+  rules: {
+    "require-jsdoc": 0,
+    "prettier/prettier": "error",
+  },
+};
