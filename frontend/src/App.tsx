76fa8c817f69b372eef806ccfad53ac4db628695
joonhan <joonhan@student.42seoul.kr>
2023-07-12 20:40:24 +0900

    🚧 [WIP]: Add PasswordResetPage #5

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 import {Route, Routes} from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
+import LoginLayout from './layout/LoginLayout';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import PasswordResetPage from './pages/PasswordResetPage';
 import NotFoundPage from './pages/NotFoundPage';
-import LoginLayout from './layout/LoginLayout';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route element={<LoginLayout />}>
           <Route path="/" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
+          <Route path="/password-reset" element={<PasswordResetPage />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
