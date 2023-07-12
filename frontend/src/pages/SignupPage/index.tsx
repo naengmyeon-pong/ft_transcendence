b91ba68375b87ce4a4e4536494db302d77ecb89c
joonhan <joonhan@student.42seoul.kr>
2023-07-12 20:06:23 +0900

    ✨ feat: Add SignupPage #5

--- frontend/src/pages/SignupPage/index.tsx
+++ frontend/src/pages/SignupPage/index.tsx
@@ -3,56 +3,154 @@ import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import {List, ListItem, ListItemIcon, ListItemText} from '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
 
 function SignupPage() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
   return (
-    <div
-      style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}
-    >
-      <h1>회원가입</h1>
-      <div style={{display: 'flex'}}>
-        <img
-          src="/Naengmyeon.png"
-          style={{objectFit: 'cover', width: '40px'}}
-          alt="냉면사진"
-        />
-        <div style={{display: 'flex', flexDirection: 'column'}}>
-          <span>프로필 사진</span>
-          <span>기본 이미지는 인트라 이미지로 설정됩니다</span>
-        </div>
-      </div>
-      {/* intraId, password */}
-      <form style={{display: 'flex', flexDirection: 'column'}}>
-        <div style={{display: 'flex', flexDirection: 'column'}}>
-          <span>intraID</span>
-          <span>naengmyeon</span>
-        </div>
-        <input type="password" placeholder="비밀번호" />
-        <input type="password" placeholder="비밀번호재확인" />
-        <span>비밀번호는 아래의 규칙에 맞게 입력해주세요</span>
-        <div style={{display: 'flex'}}>
-          <input type="checkbox" />
-          <span>전체 길이 8~20자 이내</span>
-        </div>
-        <div style={{display: 'flex'}}>
-          <input type="checkbox" />
-          <span>영문 대/소문자, 숫자, 특수문자 포함</span>
-        </div>
-        <div style={{display: 'flex'}}>
-          <input type="text" placeholder="닉네임" />
-          {/* 중복확인할 기능과 상태 */}
-          <button>중복확인</button>
-        </div>
-        <div style={{display: 'flex'}}>
-          <div style={{display: 'flex', flexDirection: 'column'}}>
-            <span>2차 인증 활성화</span>
-            <span>Google Authenticator로 추가 인증합니다</span>
-          </div>
-          <input type="checkbox" />
-        </div>
-        <button type="submit">가입하기</button>
-      </form>
-    </div>
+    <React.Fragment>
+      <Typography component="h1" variant="h5">
+        회원가입
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Card variant="outlined">
+              <CardHeader
+                avatar={<Avatar src={'인트라이미지'} />}
+                title="프로필 사진"
+                subheader="기본 이미지는 인트라 이미지로 설정됩니다"
+              />
+              <Box display="flex" justifyContent="flex-end">
+                <Button sx={{color: 'grey'}}>제거</Button>
+                <Button>업로드</Button>
+              </Box>
+            </Card>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              disabled
+              fullWidth
+              id="intraId"
+              name="intraId"
+              label="Intra ID"
+              defaultValue="API에서 가져온 인트라ID"
+              variant="filled"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              id="password"
+              name="password"
+              label="비밀번호"
+              type="password"
+              autoComplete="current-password"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              id="passwordConfirm"
+              name="passwordConfirm"
+              label="비밀번호 재확인"
+              type="password"
+              autoComplete="current-password"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <List
+              sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}
+            >
+              <ListItem color="success">
+                <ListItemIcon>
+                  <CheckIcon
+                    // color={
+                    //   isLengthGood && firstPassword !== ''
+                    //     ? 'success'
+                    //     : 'disabled'
+                    // }
+                    color="success"
+                  ></CheckIcon>
+                </ListItemIcon>
+                {/* TODO: 규칙 준수 여부에 따라 텍스트 색상 변경 */}
+                <ListItemText
+                  disableTypography
+                  secondary={
+                    <Typography variant="body2" color="text.secondary">
+                      8 ~ 20자 사이
+                    </Typography>
+                  }
+                />
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <CheckIcon
+                    // color={
+                    //   isRuleGood && firstPassword !== '' ? 'success' : 'disabled'
+                    // }
+                    color="success"
+                  ></CheckIcon>
+                </ListItemIcon>
+                {/* TODO: 규칙 준수 여부에 따라 텍스트 색상 변경 */}
+                <ListItemText
+                  disableTypography
+                  secondary={
+                    <Typography variant="body2" color="text.secondary">
+                      영문 대/소문자, 숫자, 특수문자 조합
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            </List>
+          </Grid>
+          <Grid item xs={9}>
+            <TextField
+              autoComplete="given-name"
+              name="nickanme"
+              required
+              fullWidth
+              id="nickanme"
+              label="닉네임"
+              helperText="2 ~ 8자 이내로 설정"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={3} container justifyContent="flex-end">
+            <Button variant="text">중복 확인</Button>
+          </Grid>
+          <Grid item xs={10}>
+            <Typography variant="body1" component="div">
+              2차 인증 활성화
+            </Typography>
+            <Typography sx={{mb: 1.5}} color="text.secondary">
+              Google Authenticator 로 추가 인증합니다.
+            </Typography>
+          </Grid>
+          <Grid item xs={2} container>
+            <Checkbox />
+          </Grid>
+        </Grid>
+        <Button type="submit" fullWidth variant="contained" sx={{mt: 3, mb: 2}}>
+          회원가입
+        </Button>
+      </Box>
+    </React.Fragment>
   );
 }
 
