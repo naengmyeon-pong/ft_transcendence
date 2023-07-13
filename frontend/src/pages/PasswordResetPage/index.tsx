812841ee598ccbfed7f78a69629574f355b251cb
apfp77 <apfp77@naver.com>
2023-07-13 13:06:51 +0900

    feat: PasswordResetPage UI

--- frontend/src/pages/PasswordResetPage/index.tsx
+++ frontend/src/pages/PasswordResetPage/index.tsx
@@ -1,44 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {List, ListItem, ListItemIcon, ListItemText} from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
-import Checkbox from '@mui/material/Checkbox';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
+import {useNavigate} from 'react-router-dom';
 
 function PasswordResetPage() {
+  const [passwd, setPasswd] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const navi = useNavigate();
+
+  function validatePasswordLength(password: string): boolean {
+    // 8~20자 제한
+    if (password.length > 7 && password.length < 21) {
+      return true;
+    }
+    return false;
+  }
+
+  function validatePasswordChar(password: string): boolean {
+    // 대문자, 소문자, 특수문자 각각 하나 이상
+    const regex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/;
+    return regex.test(password);
+  }
+
+  //성공했을경우만 버튼이 활성화가 됩니다
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+    console.log(data.get('password'), data.get('passwordConfirm'));
+    // TODO: 위치지정
+    navi('/');
+    //실패했을 경우 지정해줘야함
   };
+
+  function handlePasswd(e: React.ChangeEvent<HTMLInputElement>) {
+    setPasswd(e.target.value);
+  }
+  function handlePasswordConfirm(e: React.ChangeEvent<HTMLInputElement>) {
+    setPasswordConfirm(e.target.value);
+  }
+
   return (
     <React.Fragment>
       <Typography component="h1" variant="h5">
         비밀번호 재설정
       </Typography>
       <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Card variant="outlined">
-              <CardHeader
-                avatar={<Avatar src={'인트라이미지'} />}
-                title="프로필 사진"
-                subheader="기본 이미지는 인트라 이미지로 설정됩니다"
-              />
-              <Box display="flex" justifyContent="flex-end">
-                <Button sx={{color: 'grey'}}>제거</Button>
-                <Button>업로드</Button>
-              </Box>
-            </Card>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={12} display="flex" justifyContent="center">
+            <Avatar src={'인트라이미지'} />
           </Grid>
           <Grid item xs={12}>
             <TextField
@@ -52,26 +68,89 @@ function PasswordResetPage() {
             />
           </Grid>
           <Grid item xs={12}>
-            <TextField
-              required
-              fullWidth
-              id="password"
-              name="password"
-              label="비밀번호"
-              type="password"
-              autoComplete="current-password"
-            />
+            {passwd === '' || validatePasswordLength(passwd) ? (
+              passwd === '' || validatePasswordChar(passwd) ? (
+                <TextField
+                  required
+                  fullWidth
+                  id="password"
+                  name="password"
+                  label="비밀번호"
+                  type="password"
+                  autoComplete="current-password"
+                  onChange={handlePasswd}
+                />
+              ) : (
+                <TextField
+                  error
+                  fullWidth
+                  id="password"
+                  name="password"
+                  label="비밀번호"
+                  type="password"
+                  autoComplete="current-password"
+                  onChange={handlePasswd}
+                  helperText="비밀번호는 영문 대/소문자, 숫자, 특수문자 조합이여야 합니다."
+                />
+              )
+            ) : (
+              <TextField
+                error
+                fullWidth
+                id="password"
+                name="password"
+                label="비밀번호"
+                type="password"
+                autoComplete="current-password"
+                onChange={handlePasswd}
+                helperText="비밀번호는 8 ~ 20 사이입니다."
+              />
+            )}
           </Grid>
           <Grid item xs={12}>
-            <TextField
-              required
-              fullWidth
-              id="passwordConfirm"
-              name="passwordConfirm"
-              label="비밀번호 재확인"
-              type="password"
-              autoComplete="current-password"
-            />
+            {passwordConfirm === '' ||
+            validatePasswordLength(passwordConfirm) ? (
+              passwordConfirm === '' ||
+              validatePasswordChar(passwordConfirm) ? (
+                <TextField
+                  required
+                  fullWidth
+                  id="passwordConfirm"
+                  name="passwordConfirm"
+                  label="비밀번호 재확인"
+                  type="password"
+                  autoComplete="current-password"
+                  onChange={handlePasswordConfirm}
+                />
+              ) : (
+                <TextField
+                  error
+                  fullWidth
+                  id="passwordConfirm"
+                  name="passwordConfirm"
+                  label="비밀번호 재확인"
+                  type="password"
+                  autoComplete="current-password"
+                  onChange={handlePasswordConfirm}
+                  helperText="비밀번호는 영문 대/소문자, 숫자, 특수문자 조합이여야 합니다."
+                />
+              )
+            ) : (
+              <TextField
+                error
+                fullWidth
+                id="passwordConfirm"
+                name="passwordConfirm"
+                label="비밀번호 재확인"
+                type="password"
+                autoComplete="current-password"
+                onChange={handlePasswordConfirm}
+                helperText="비밀번호는 8 ~ 20 사이입니다."
+              />
+            )}
+          </Grid>
+          <Grid item xs={12}>
+            <Typography>비밀번호는 아래의 규칙에 맞게 입력해주세요.</Typography>
           </Grid>
           <Grid item xs={12}>
             <List
@@ -85,12 +164,16 @@ function PasswordResetPage() {
                     //     ? 'success'
                     //     : 'disabled'
                     // }
-                    color="success"
+                    color={
+                      validatePasswordLength(passwd) ? 'success' : 'disabled'
+                    }
+                    // color="success"
                   ></CheckIcon>
                 </ListItemIcon>
                 {/* TODO: 규칙 준수 여부에 따라 텍스트 색상 변경 */}
                 <ListItemText
                   disableTypography
+                  // secondary={
                   secondary={
                     <Typography variant="body2" color="text.secondary">
                       8 ~ 20자 사이
@@ -104,7 +187,10 @@ function PasswordResetPage() {
                     // color={
                     //   isRuleGood && firstPassword !== '' ? 'success' : 'disabled'
                     // }
-                    color="success"
+                    color={
+                      validatePasswordChar(passwd) ? 'success' : 'disabled'
+                    }
+                    // color="success"
                   ></CheckIcon>
                 </ListItemIcon>
                 {/* TODO: 규칙 준수 여부에 따라 텍스트 색상 변경 */}
@@ -119,36 +205,23 @@ function PasswordResetPage() {
               </ListItem>
             </List>
           </Grid>
-          <Grid item xs={9}>
-            <TextField
-              autoComplete="given-name"
-              name="nickanme"
-              required
-              fullWidth
-              id="nickanme"
-              label="닉네임"
-              helperText="2 ~ 8자 이내로 설정"
-              autoFocus
-            />
-          </Grid>
-          <Grid item xs={3} container justifyContent="flex-end">
-            <Button variant="text">중복 확인</Button>
-          </Grid>
-          <Grid item xs={10}>
-            <Typography variant="body1" component="div">
-              2차 인증 활성화
-            </Typography>
-            <Typography sx={{mb: 1.5}} color="text.secondary">
-              Google Authenticator 로 추가 인증합니다.
-            </Typography>
-          </Grid>
-          <Grid item xs={2} container>
-            <Checkbox />
-          </Grid>
         </Grid>
-        <Button type="submit" fullWidth variant="contained" sx={{mt: 3, mb: 2}}>
-          회원가입
-        </Button>
+        {passwd === passwordConfirm &&
+        validatePasswordChar(passwd) &&
+        validatePasswordLength(passwd) ? (
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{mt: 3, mb: 2}}
+          >
+            수정하기
+          </Button>
+        ) : (
+          <Button disabled fullWidth variant="contained" sx={{mt: 3, mb: 2}}>
+            수정하기
+          </Button>
+        )}
       </Box>
     </React.Fragment>
   );
