ba5dd077394ed7e2d715eaf050a6fcda4217e333
joonhan <joonhan@student.42seoul.kr>
2023-07-13 18:55:57 +0900

    ⚰️ feat: Remove apiManager interceptors #8
    Co-authored-by: apfp77 <apfp77@users.noreply.github.com>
    Co-authored-by: Rayd-kim <Rayd-kim@users.noreply.github.com>
    Co-authored-by: Taehoon Kim <taehoonkim.dev@gmail.com>

--- frontend/src/api/apiManager.ts
+++ frontend/src/api/apiManager.ts
@@ -1,22 +1,22 @@
-import axios from 'axios';
+import axios, {AxiosInstance} from 'axios';
 
-const apiManager = axios.create({
-  baseURL: `https://${process.env.REACT_APP_AWS_BACKEND_SERVER}`,
+const apiManager: AxiosInstance = axios.create({
+  baseURL: `${process.env.REACT_APP_BACKEND_SERVER}`,
   timeout: 3000,
 });
 
-apiManager.interceptors.request.use(
-  config => {
-    const localStorageToken = localStorage.getItem('accessToken');
-    const jwtToken =
-      localStorageToken === null ? '' : `Bearer ${localStorageToken}`;
-    config.headers['authorization'] = jwtToken;
-    return config;
-  },
-  error => {
-    console.log(error);
-    return Promise.reject(error);
-  }
-);
+// apiManager.interceptors.request.use(
+//   config => {
+//     const localStorageToken = localStorage.getItem('accessToken');
+//     const jwtToken =
+//       localStorageToken === null ? '' : `Bearer ${localStorageToken}`;
+//     config.headers['authorization'] = jwtToken;
+//     return config;
+//   },
+//   error => {
+//     console.log(error);
+//     return Promise.reject(error);
+//   }
+// );
 
 export default apiManager;
