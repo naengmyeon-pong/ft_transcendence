93ed82022562aa986ec52c97a0e2145d93a449e3
Taehoon Kim <taehoonkim.dev@gmail.com>
2023-07-14 17:06:20 +0900

    docs: swagger를 사용한 api 명세 작성#17

--- backend/src/user/dto/user.dto.ts
+++ backend/src/user/dto/user.dto.ts
@@ -5,12 +5,23 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import {ApiProperty} from '@nestjs/swagger';
 
 export class UserDto {
+  @ApiProperty({
+    example: 'my_id',
+    description: '유저의 인트라 id',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   user_id: string;
 
+  @ApiProperty({
+    example: '!123Qwerty',
+    description: '사용할 비밀번호',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   @MinLength(8)
@@ -20,12 +31,27 @@ export class UserDto {
   })
   user_pw: string;
 
+  @ApiProperty({
+    example: 'genius',
+    description: '사용할 닉네임',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   @MaxLength(10)
   user_nickname: string;
 
+  @ApiProperty({
+    example: 'https://cdn.intra.42.fr/users/1234/test.jpg',
+    description: '이미지 주소',
+    required: true,
+  })
   user_image: string;
 
+  @ApiProperty({
+    example: 'false',
+    description: '2차 인증 사용 여부',
+    required: true,
+  })
   is_2fa_enabled: boolean;
 }
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -13,7 +13,7 @@ import {
 } from '@nestjs/common';
 import {UserService} from './user.service';
 import {User} from './user.entitiy';
-import {ApiTags} from '@nestjs/swagger';
+import {ApiTags, ApiOperation, ApiQuery} from '@nestjs/swagger';
 import {UserDto} from './dto/user.dto';
 import {UserAuthDto} from './dto/userAuth.dto';
 
@@ -28,11 +28,24 @@ export class UserController {
   }
 
   @Delete('/delete:user_id')
+  @ApiQuery({
+    name: 'user_id',
+    required: true,
+    description: '회원 정보 제거할 유저의 id',
+  })
+  @ApiOperation({
+    summary: '회원 정보 삭제 API',
+    description: 'user_id에 해당하는 회원 정보를 DB에서 삭제한다.',
+  })
   remove(@Param('user_id') user_id: string): Promise<void> {
     return this.userService.remove(user_id);
   }
 
   @Post('/changePw')
+  @ApiOperation({
+    summary: '비밀번호 변경 API',
+    description: '기존 암호를 사용자로부터 전달받은 새로운 암호로 변경한다.',
+  })
   updateUserPw(@Body(ValidationPipe) userAuthDto: UserAuthDto): Promise<User> {
     return this.userService.updateUserPw(userAuthDto);
   }
