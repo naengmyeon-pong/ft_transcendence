ea72950d72de10670a63b51af951b3b96c96022c
joonhan <joonhan@student.42seoul.kr>
2023-07-18 18:45:43 +0900

    ✨ feat: Add user nickname and image #28

--- frontend/src/layout/Buffer/SideBar/Profile.tsx
+++ frontend/src/layout/Buffer/SideBar/Profile.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
 import {Box, Typography} from '@mui/material';
+import apiManager from '@apiManager/apiManager';
 
 export default function Profile() {
-  const userName = '방글맨방글방글맨';
   const rankNum = '랭크점수: 1000';
+
+  const [nickname, setNickname] = useState('');
+  const [userImage, setUserImage] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        console.log('hello');
+        const response = await apiManager.get('/user/user-info');
+        console.log(response);
+        const {user_nickname, user_image} = response.data;
+        setNickname(user_nickname);
+        setUserImage(`http://localhost:3001${user_image}`);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
   return (
     <Box
       sx={{
@@ -18,13 +38,13 @@ export default function Profile() {
       <Box display="flex">
         <Typography variant="h6" noWrap component="p">
           <img
-            src="/Naengmyeon.png"
+            src={userImage}
             style={{objectFit: 'cover', width: '40px'}}
-            alt="프로"
+            alt="프로필사진"
           />
         </Typography>
         <Box sx={{px: '10px'}}>
-          <Typography variant="h6">{userName}</Typography>
+          <Typography variant="h6">{nickname}</Typography>
           <Typography variant="h6" sx={{fontSize: '1em'}}>
             {rankNum}
           </Typography>
