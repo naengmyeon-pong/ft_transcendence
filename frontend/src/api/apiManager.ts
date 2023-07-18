a74cdf29904b87ba51af23cc4135a8347f3d83e1
joonhan <joonhan@student.42seoul.kr>
2023-07-18 18:44:22 +0900

    ✨ feat: Add jwtToken #28

--- frontend/src/api/apiManager.ts
+++ frontend/src/api/apiManager.ts
@@ -5,18 +5,18 @@ const apiManager: AxiosInstance = axios.create({
   timeout: 3000,
 });
 
-// apiManager.interceptors.request.use(
-//   config => {
-//     const localStorageToken = localStorage.getItem('accessToken');
-//     const jwtToken =
-//       localStorageToken === null ? '' : `Bearer ${localStorageToken}`;
-//     config.headers['authorization'] = jwtToken;
-//     return config;
-//   },
-//   error => {
-//     console.log(error);
-//     return Promise.reject(error);
-//   }
-// );
+apiManager.interceptors.request.use(
+  config => {
+    const sessionStorageToken = sessionStorage.getItem('accessToken');
+    const jwtToken =
+      sessionStorageToken === null ? '' : `Bearer ${sessionStorageToken}`;
+    config.headers['authorization'] = jwtToken;
+    return config;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
 export default apiManager;
