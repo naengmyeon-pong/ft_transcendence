49fc8e0b8fdf4526dfd0d6943bcc5a26ad2b0e1a
joonhan <joonhan@student.42seoul.kr>
2023-07-31 20:07:50 +0900

    🚧 WIP: 게임모드 선택 레이아웃 작성

--- frontend/src/pages/GamePage/index.tsx
+++ frontend/src/pages/GamePage/index.tsx
@@ -1,10 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import {io, Socket} from 'socket.io-client';
+import {Box, Grid} from '@mui/material';
 import Pong from './Pong';
 
 import {GameInfo, RoomUserInfo, JoinGameInfo} from '@/types/game';
 
-const socket = io('http://localhost:3001/game');
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 500;
+
+// const socket: Socket = io('http://localhost:3001/game');
 
 const handleBeforeUnload = (e: BeforeUnloadEvent) => {
   e.preventDefault();
@@ -29,9 +33,9 @@ function Game() {
     sessionStorage.setItem('left_user', left_user);
     sessionStorage.setItem('right_user', right_user);
 
-    if (socket) {
-      socket.emit('update_frame', room_name);
-    }
+    // if (socket) {
+    //   socket.emit('update_frame', room_name);
+    // }
   };
 
   const handleGameInfo = ({game_info}: {game_info: GameInfo}) => {
@@ -45,11 +49,11 @@ function Game() {
       mode: 'easy',
       type: 'normal',
     };
-    socket.emit('join_game', joinGameInfo);
+    // socket.emit('join_game', joinGameInfo);
 
-    socket.on('notice', handleNotice);
-    socket.on('room_name', handleRoomname);
-    socket.on('game_info', handleGameInfo);
+    // socket.on('notice', handleNotice);
+    // socket.on('room_name', handleRoomname);
+    // socket.on('game_info', handleGameInfo);
     window.addEventListener('beforeunload', handleBeforeUnload);
 
     sessionStorage.removeItem('room_name');
@@ -57,17 +61,28 @@ function Game() {
     sessionStorage.removeItem('right_user');
 
     return () => {
-      socket.off('notice', handleNotice);
-      socket.off('room_name', handleRoomname);
-      socket.off('game_info', handleGameInfo);
+      // socket.off('notice', handleNotice);
+      // socket.off('room_name', handleRoomname);
+      // socket.off('game_info', handleGameInfo);
       window.removeEventListener('beforeunload', handleBeforeUnload);
     };
   }, []);
 
   return (
     <>
-      <p>game</p>
-      <Pong socket={socket} gameInfo={gameInfo} />
+      <Grid id="game-area" container justifyContent="center" xs={12}>
+        <Grid id="mode-selection" container justifyContent="center" xs={12}>
+          <Grid xs={6}>
+            <p>123</p>
+          </Grid>
+          <Grid xs={6}>
+            <p>123</p>
+          </Grid>
+        </Grid>
+        <Grid xs={8}>
+          <Pong gameInfo={gameInfo} />
+        </Grid>
+      </Grid>
     </>
   );
 }
