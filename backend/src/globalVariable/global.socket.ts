2a3fb549481b7f26086918fd4131433b5f0a0cd6
Taehoon Kim <taehoonkim.dev@gmail.com>
2023-08-18 18:41:44 +0900

    refactor: 소켓 전역변수에 게임 소켓 추가

--- backend/src/globalVariable/global.socket.ts
+++ backend/src/globalVariable/global.socket.ts
@@ -1,13 +1,21 @@
 import {Injectable} from '@nestjs/common';
+import {Socket} from 'socket.io';
 
 interface UserSocket {
   user_id: string;
   socket_id: string;
 }
 
+interface SocketInfo {
+  chat_socket: string | null;
+  game_socket: string | null;
+  is_gaming: boolean;
+}
+
 @Injectable()
 export class SocketArray {
-  private socketArray = new Map<string, string>();
+  private socketArray = new Map<string, SocketInfo>();
+  // private socketArray = new Map<string, string>();
 
   getSocketArray() {
     return this.socketArray;
@@ -21,7 +29,39 @@ export class SocketArray {
     this.socketArray.delete(user_id);
   }
 
-  addSocketArray(userSocket: UserSocket) {
-    this.socketArray.set(userSocket.user_id, userSocket.socket_id);
+  // addSocketArray(userSocket: UserSocket) {
+  //   this.socketArray.set(userSocket.user_id, userSocket.socket_id);
+  // }
+
+  addChatSocketArray(userSocket: UserSocket) {
+    let socketInfo: SocketInfo | undefined = this.socketArray.get(
+      userSocket.user_id
+    );
+    if (socketInfo !== undefined) {
+      socketInfo.chat_socket = userSocket.socket_id;
+      return;
+    }
+    socketInfo = {
+      chat_socket: userSocket.socket_id,
+      game_socket: null,
+      is_gaming: false,
+    };
+    this.socketArray.set(userSocket.user_id, socketInfo);
+  }
+
+  addGameSocketArray(userSocket: UserSocket) {
+    let socketInfo: SocketInfo | undefined = this.socketArray.get(
+      userSocket.user_id
+    );
+    if (socketInfo !== undefined) {
+      socketInfo.game_socket = userSocket.socket_id;
+      return;
+    }
+    socketInfo = {
+      chat_socket: null,
+      game_socket: userSocket.socket_id,
+      is_gaming: false,
+    };
+    this.socketArray.set(userSocket.user_id, socketInfo);
   }
 }
