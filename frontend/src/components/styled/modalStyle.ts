2e206884a163c3107f4fbf6390279c334c5590bf
joonhan <joonhan@student.42seoul.kr>
2023-08-19 18:27:15 +0900

    ✨ feat: 모달 너비 600px 로 증가

--- frontend/src/components/styled/modalStyle.ts
+++ frontend/src/components/styled/modalStyle.ts
@@ -3,7 +3,7 @@ export const modalStyle = {
   top: '40%',
   left: '48%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 600,
   bgcolor: 'background.paper',
   border: '2px solid #FFF',
   boxShadow: 24,
