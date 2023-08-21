4dcb0a0a1ff4b07b6ae23a889804b1db5dde4b70
23tae <taehoonkim.dev@gmail.com>
2023-08-21 21:24:49 +0900

    chore: 미사용 쿼리문 삭제

--- backend/test_query.sql
+++ backend/test_query.sql
@@ -4,13 +4,3 @@ VALUES
   ('user2', '1234', 'nick2', '/images/logo.jpeg', false, 1000),
   ('user3', '1234', 'nick3', '/images/logo.jpeg', false, 1000),
   ('user4', '1234', 'nick4', '/images/logo.jpeg', false, 1000);
-
-INSERT INTO game_type (id, type)
-VALUES 
-  (1, '일반'),
-  (2, '랭크');
-
-INSERT INTO game_mode (id, mode)
-VALUES 
-  (1, '일반'),
-  (2, '가속');
