7d501f94a7b7c6e17dbe90b3797ba76bcf7158b5
23tae <taehoonkim.dev@gmail.com>
2023-08-21 21:32:09 +0900

    chore: gameuser 인터페이스 변수 추가

--- backend/src/game/types/game-user.interface.ts
+++ backend/src/game/types/game-user.interface.ts
@@ -1,9 +1,9 @@
-import {Socket} from 'socket.io';
-import {KeyData} from './key-data.interface';
+import { Socket } from 'socket.io';
+import { KeyData } from './key-data.interface';
 
 export interface GameUser {
   user_id: string;
-  socket: Socket;
+  socket_id: string;
   keys: KeyData;
   type_mode: number;
 }
