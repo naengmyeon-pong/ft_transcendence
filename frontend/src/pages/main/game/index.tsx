ffb6d9c22616b1bd70a58033ce3366bebd31f320
Taehoon Kim <taehoonkim.dev@gmail.com>
2023-08-24 21:24:06 +0900

    feat: 게임 페이지에서 이탈하는 경우 exit_game 이벤트 발생 추가

--- frontend/src/pages/main/game/index.tsx
+++ frontend/src/pages/main/game/index.tsx
@@ -1,12 +1,24 @@
 'use client';
 
+import {useEffect, useContext} from 'react';
+import {useRecoilValue} from 'recoil';
+
 import {inviteGameState} from '@/states/inviteGame';
 import Game from '@/components/game/Game';
 import InviteGame from '@/components/game/InviteGame';
-import {useRecoilValue} from 'recoil';
+import {UserContext} from '@/components/layout/MainLayout/Context';
 
 function GameManager() {
   const invite_game_state = useRecoilValue(inviteGameState);
+  const {chat_socket} = useContext(UserContext);
+  
+  useEffect(() => {
+    return () => {
+      if (chat_socket) {
+        chat_socket.emit('exit_game');
+      }
+    }
+  }, []);
 
   return <>{invite_game_state === null ? <Game /> : <InviteGame />}</>;
 }
