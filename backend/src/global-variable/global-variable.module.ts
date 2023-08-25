4a51e096efebf8f64600ffc1c3f5ec525aadc46e
Rayd-kim <91304459+Rayd-kim@users.noreply.github.com>
2023-08-25 13:45:18 +0900

    feat: friend 전역클래스 추가 (#114)

--- backend/src/global-variable/global-variable.module.ts
+++ backend/src/global-variable/global-variable.module.ts
@@ -1,14 +1,24 @@
 import {Global, Module} from '@nestjs/common';
 import {SocketArray} from './global.socket';
 import {Block} from './global.block';
-import {BlockList} from '@/chat/chat.entity';
+import {BlockList, FriendList} from '@/chat/chat.entity';
 import {TypeOrmModule} from '@nestjs/typeorm';
-import {BlockRepository} from '@/chat/chat.repository';
+import {BlockRepository, FriendListRepository} from '@/chat/chat.repository';
+import {Friend} from './global.friend';
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([BlockList])],
-  providers: [SocketArray, Block, BlockRepository],
-  exports: [SocketArray, Block],
+  imports: [
+    TypeOrmModule.forFeature([BlockList]),
+    TypeOrmModule.forFeature([FriendList]),
+  ],
+  providers: [
+    SocketArray,
+    Block,
+    Friend,
+    BlockRepository,
+    FriendListRepository,
+  ],
+  exports: [SocketArray, Block, Friend],
 })
 export class GlobalVariableModule {}
---
+++ backend/src/global-variable/global.friend.ts
@@ -0,0 +1,38 @@
+import {Injectable} from '@nestjs/common';
+import {FriendListRepository} from '@/chat/chat.repository';
+
+// 나를 친구추가한 사람들
+@Injectable()
+export class Friend {
+  private friend = new Map<string, Set<string>>();
+  constructor(private friendListRepository: FriendListRepository) {}
+
+  async setFriend() {
+    const friend_list = await this.friendListRepository.find();
+    friend_list.forEach(e => {
+      this.addFriendUser(e.userId, e.friendId);
+    });
+  }
+
+  getFriendUsers(user_id: string): Set<string> {
+    const ret: Set<string> = this.friend.get(user_id);
+    return ret;
+  }
+
+  addFriendUser(user_id: string, friend_id: string): void {
+    const user = this.friend.get(friend_id);
+    if (user) {
+      user.add(user_id);
+    } else {
+      this.friend.set(friend_id, new Set());
+      this.friend.get(friend_id).add(user_id);
+    }
+  }
+
+  removeFriendUser(user_id: string, friend_id: string): void {
+    const user = this.friend.get(friend_id);
+    if (user) {
+      user.delete(user_id);
+    }
+  }
+}
