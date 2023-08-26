1a324585e4a09b344f99e785b98c3b8caa1c1fa0
apfp77 <apfp77@naver.com>
2023-08-26 18:35:00 +0900

    BUG: 랜더링 문제로 서버에 이벤트 발송 수정

--- frontend/src/components/game/InviteGame.tsx
+++ frontend/src/components/game/InviteGame.tsx
@@ -7,40 +7,16 @@ import {useRecoilState} from 'recoil';
 import {inviteGameState} from '@/states/inviteGame';
 import {Button, Grid} from '@mui/material';
 import Pong from './Pong';
+import {useGlobalModal} from '@/hooks/useGlobalModal';
 
-interface InviteGameViewType {
-  isGameOver: boolean;
-  gameInfo: GameInfo;
-  handleReturnMain: () => void;
-}
-
-function InviteGameView({
-  isGameOver,
-  gameInfo,
-  handleReturnMain,
-}: InviteGameViewType) {
+function InviteGameView({gameInfo}: {gameInfo: GameInfo}) {
   const {chat_socket} = useContext(UserContext);
 
   return (
     <>
-      {!isGameOver && (
-        <Grid item xs={8}>
-          <Pong socket={chat_socket} gameInfo={gameInfo} />
-        </Grid>
-      )}
-      {isGameOver && gameInfo !== null && (
-        <Grid item>
-          <p>
-            {gameInfo.leftScore > gameInfo.rightScore
-              ? sessionStorage.getItem('left_user')
-              : sessionStorage.getItem('right_user')}
-            승리!
-          </p>
-          <Button variant="contained" onClick={handleReturnMain}>
-            메인으로 돌아가기
-          </Button>
-        </Grid>
-      )}
+      <Grid item xs={8}>
+        <Pong socket={chat_socket} gameInfo={gameInfo} />
+      </Grid>
     </>
   );
 }
@@ -48,29 +24,31 @@ function InviteGameView({
 export default function InviteGame() {
   const {chat_socket, user_id} = useContext(UserContext);
   const [gameInfo, setGameInfo] = useState<GameInfo | null>(null);
-  const [isGameOver, setIsGameOver] = useState<boolean>(false);
   const [invite_game_state, setInviteGameState] =
     useRecoilState(inviteGameState);
   const start_geme_prev_unload = useRef(true);
-
-  const handleReturnMain = () => {
-    setIsGameOver(false);
-    setGameInfo(null);
-    setInviteGameState(null);
-  };
+  const {openGlobalModal, closeGlobalModal} = useGlobalModal();
 
   const handleInviteGameInfo = useCallback(
     ({game_info}: {game_info: GameInfo}) => {
       if (game_info === null) {
         return;
       }
-
       setGameInfo(game_info);
       if (game_info.leftScore === 5 || game_info.rightScore === 5) {
-        setIsGameOver(true);
+        const winner =
+          game_info.leftScore > game_info.rightScore
+            ? sessionStorage.getItem('left_user')
+            : sessionStorage.getItem('right_user');
+        openGlobalModal({
+          title: `${winner}님이 승리하였습니다`,
+          content: <></>,
+          action: <Button onClick={closeGlobalModal}>닫기</Button>,
+        });
+        setInviteGameState(null);
       }
     },
-    []
+    [openGlobalModal, closeGlobalModal, setInviteGameState]
   );
 
   const sendGameStartEvent = useCallback(() => {
@@ -107,6 +85,9 @@ export default function InviteGame() {
     window.addEventListener('beforeunload', handleBeforeUnload);
 
     return () => {
+      sessionStorage.removeItem('left_user');
+      sessionStorage.removeItem('right_user');
+      sessionStorage.removeItem('room_name');
       window.removeEventListener('unload', handleUnload);
       window.removeEventListener('beforeunload', handleBeforeUnload);
       setInviteGameState(null);
@@ -114,9 +95,15 @@ export default function InviteGame() {
   }, [chat_socket, invite_game_state, setInviteGameState, handleUnload]);
 
   useEffect(() => {
+    chat_socket?.on('game_info', handleInviteGameInfo);
+    return () => {
+      chat_socket?.off('game_info', handleInviteGameInfo);
+    };
+  }, [chat_socket, handleInviteGameInfo]);
+
+  useEffect(() => {
     // 게임 대기방에 들어왔다는 신호
     chat_socket?.on('enter_game', sendGameStartEvent);
-    chat_socket?.on('game_info', handleInviteGameInfo);
     // 초대자가 게임을 거절한 경우
     chat_socket?.on('inviter_cancel_game_refuse', exitCancelGame);
     // 초대자가 초대 후 게임을 시작한경우
@@ -133,11 +120,11 @@ export default function InviteGame() {
     return () => {
       chat_socket?.off('inviter_cancel_game_refresh', exitCancelGame);
       chat_socket?.off('enter_game', sendGameStartEvent);
-      chat_socket?.off('game_info', handleInviteGameInfo);
       chat_socket?.off('inviter_cancel_game_refuse', exitCancelGame);
       chat_socket?.off('inviter_cancel_game_betray', exitCancelGame);
       chat_socket?.off('start_game');
       if (start_geme_prev_unload.current) {
+        console.log('??');
         chat_socket?.emit('invitee_cancel_game_back', invite_game_state);
       }
     };
@@ -145,17 +132,10 @@ export default function InviteGame() {
     chat_socket,
     user_id,
     invite_game_state.inviter_id,
-    handleInviteGameInfo,
     sendGameStartEvent,
     invite_game_state,
     exitCancelGame,
   ]);
 
-  return (
-    <InviteGameView
-      isGameOver={isGameOver}
-      gameInfo={gameInfo}
-      handleReturnMain={handleReturnMain}
-    />
-  );
+  return <InviteGameView gameInfo={gameInfo} />;
 }
